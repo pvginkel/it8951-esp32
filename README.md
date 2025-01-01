89b4b3bab6973c06262cb275fffb6e369b0d1d25
Pieter van Ginkel <pvginkel@gmail.com>
2025-01-01 22:09:48 +0100

    Added documentation.

---
+++ README.md
@@ -0,0 +1,109 @@
+# IT8951 driver for the ESP32
+
+This is a driver for the ESP32 for the IT8951 controller for e-Reader screens.
+It's used for a number of high performance e-Reader screens from Waveshare.
+
+The examples folders contain examples for how to use this driver.
+
+## Setup
+
+Setup is done using `sdkconfig` to select the SPI host and pins, and using
+the `setup()` method to set the VCOM voltage. The value for this is printed
+on the cable of the e-Reader screen. It's important that it's correct.
+
+## Showing images on the screen
+
+Images need to be uploaded to the controller before they can be shown
+on the screen. Images can be any size and don't have to fit the whole
+screen. This is especially interesting when using the A2 fast update mode
+because you can update small parts of the screen quite fast without
+any flickering.
+
+Uploading an image is done as follows. First define the size of the image
+to show. In this example, the image is the full size of the screen.
+
+```cpp
+IT8951Area area = {
+    .x = 0,
+    .y = 0,
+    .w = display.get_width(),
+    .h = display.get_height(),
+};
+```
+
+The image is then transferred to the controller. In this example, there's a
+display buffer that's the full size of the screen. The pixel format is 1 bit
+per pixel, so the buffer is `height * width / 8` bytes in size. That's very doable
+on an ESP32-S3 with 2 MB of PSIRAM. In that case, the screen buffer can be
+copied to the controller in one go.
+
+If however you want to show a 16 color gray scale image, this becomes challenging.
+An alternative approach is to use LVGL's partial rendering to render the screen
+in chunks. In this case, you call `load_image_start()` when LVGL starts rendering
+the screen and copy the part of the screen that's been rendered to the controller.
+Only when the whole screen is updated do you call `load_image_end()` and display
+the image.
+
+Regardless the pattern you implement, the logic is like the logic below. It
+works as follows.
+
+First, call `load_image_start()` to signal that an image is going to be copied
+to the controller. This takes the dimensions of the image, the target address
+in in the memory space on the controller (always use `get_memory_address()` for
+this unless you want to do something fancy), the rotation (the controller supports
+hardware based rotation) and the pixel format.
+
+```cpp
+display.load_image_start(area, display.get_memory_address(), IT8951_ROTATE_0, IT8951_PIXEL_FORMAT_1BPP);
+```
+
+They copy the image to the controller. The driver has two buffers to transfer
+data using SPI. While one buffer is being transferred by the SPI host, the CPU
+will be filling up the other buffer. `load_image_flush_buffer()` signals that
+the new buffer is ready to be transferred, will wait for the first buffer
+to complete transfer and will start copying the second buffer.
+
+```cpp
+const size_t buffer_len = display.get_buffer_len();
+
+for (size_t offset = 0; offset < display_buffer_size; offset += buffer_len) {
+    const auto copy = std::min(buffer_len, display_buffer_size - offset);
+```
+
+In this case, we can simply `memcpy()` chunks of memory from the display buffer
+to the SPI transfer buffer. If however you need to take a reference of
+the SPI buffer, make sure you refresh this reference after you call
+`load_image_flush_buffer()`.
+
+```cpp
+    memcpy(display.get_buffer(), display_buffer + offset, copy);
+
+    display.load_image_flush_buffer(copy);
+}
+```
+
+Once all data has been copied, call `load_image_end()` to signal this to
+the controller.
+
+```cpp
+display.load_image_end();
+```
+
+`display_area()` shows the image on the display. This takes the same
+area (although the coordinates need to be translated if you're using
+hardware rotation), the memory address on the controller, the pixel format
+and the display mode.
+
+Regarding the display mode, the following exist:
+
+* `IT8951_DISPLAY_MODE_INIT` clears the screen. `clear_screen()` implements
+  the correct logic for this.
+* `IT8951_DISPLAY_MODE_A2` updates the screen without doing a full refresh
+  of the screen. This is fast, but leaves an after image, and it only
+  supports 1 bit per pixel.
+* `IT8951_DISPLAY_MODE_GC16` supports 16 color gray scale, but is slower, and
+  flashes the screen on update.
+
+```cpp
+display.display_area(area, display.get_memory_address(), IT8951_PIXEL_FORMAT_1BPP, IT8951_DISPLAY_MODE_A2);
+```
--- src/include/it8951.h
+++ src/include/it8951.h
@@ -2,6 +2,9 @@
 
 #include "driver/spi_master.h"
 
+/**
+ * @brief Area identifying the size of images and display areas.
+ */
 struct IT8951Area {
     uint16_t x;
     uint16_t y;
@@ -9,6 +12,9 @@ struct IT8951Area {
     uint16_t h;
 };
 
+/**
+ * @brief Hardware rotation for images.
+ */
 enum it8951_rotate_t : uint8_t {
     IT8951_ROTATE_0 = 0,
     IT8951_ROTATE_90 = 1,
@@ -16,19 +22,28 @@ enum it8951_rotate_t : uint8_t {
     IT8951_ROTATE_270 = 3,
 };
 
+/**
+ * @brief Pixel format of images.
+ */
 enum it8951_pixel_format_t : uint8_t {
-    IT8951_PIXEL_FORMAT_1BPP,
-    IT8951_PIXEL_FORMAT_2BPP,
-    IT8951_PIXEL_FORMAT_4BPP,
-    IT8951_PIXEL_FORMAT_8BPP,
+    IT8951_PIXEL_FORMAT_1BPP,  ///< Monochrome images, required for A2 fast updates.
+    IT8951_PIXEL_FORMAT_2BPP,  ///< Four gray scale mode; use only when memory space is limited.
+    IT8951_PIXEL_FORMAT_4BPP,  ///< High fidelity gray scale mode.
+    IT8951_PIXEL_FORMAT_8BPP,  ///< Don't use. 4 bit per pixel is the highest supported anyway.
 };
 
+/**
+ * @brief Display mode to show images on the screen.
+ */
 enum it8951_display_mode_t {
-    IT8951_DISPLAY_MODE_INIT,
-    IT8951_DISPLAY_MODE_A2,
-    IT8951_DISPLAY_MODE_GC16,
+    IT8951_DISPLAY_MODE_INIT,  ///< Init mode to refresh the screen. Use `clear_screen()` instead.
+    IT8951_DISPLAY_MODE_A2,    ///< Fast display mode. Requires 1 bit per pixel images.
+    IT8951_DISPLAY_MODE_GC16,  ///< 16 color gray scale mode.
 };
 
+/**
+ * @brief Driver for the IT8951 controller.
+ */
 class IT8951 {
     struct DeviceInfo {
         uint16_t width;
@@ -40,20 +55,106 @@ class IT8951 {
     };
 
 public:
+    /**
+     * @brief Setup the controller.
+     * @param vcom The VCOM value. This has to be set correctly and is the number printed on the cable.
+     * @return Whether the controller was setup correctly.
+     */
     bool setup(float vcom);
+
+    /**
+     * @brief Get the current SPI transfer buffer. Called after `load_image_start()`.
+     * @return The current SPI transfer buffer.
+     */
     uint8_t* get_buffer() { return _current_buffer == 0 ? _buffer0 : _buffer1; }
+
+    /**
+     * @brief Gets the size of the SPI transfer buffers.
+     */
     size_t get_buffer_len() { return _buffer_len; }
+
+    /**
+     * @brief Gets the width of the screen.
+     *
+     * Note that this is not necessarily a multiple of 2 or 8. This width needs
+     * to be rounded to scan line sizes based on the pixel format of the image.
+     * For 1 bit per pixel, a scan line will be `(get_width() + 7) / 8`. For
+     * 4 bit per pixel, it will be `(get_width() + 1) / 2`.
+     */
     uint16_t get_width() { return _width; }
+
+    /**
+     * @brief Gets the height of the screen.
+     */
     uint16_t get_height() { return _height; }
+
+    /**
+     * @brief Gets the memory address where images can be stored on the controller.
+     *
+     * IT8951 controllers have quite some memory. More than one image can be
+     * stored on it. This allows for advanced functionality. If you don't need this,
+     * You can just use this base memory address as the target for images.
+     */
     uint32_t get_memory_address() { return _memory_address; }
+
+    /**
+     * @brief Enable enhanced driver capability mode. Enable this if the screen behaves
+     * funny without it.
+     */
     void enable_enhance_driving_capability();
+
+    /**
+     * @brief Wake the controller from sleep mode.
+     *
+     * The screen needs to be cleared after the system is woken up from
+     * sleep mode. Otherwise you get strange artifacts on the screen. If you
+     * get the controller to work without clearing the screen, raise a GitHub
+     * issue so that the documentation can be updated.
+     */
     void set_system_run();
+
+    /**
+     * @brief Put the controller in sleep mode.
+     */
     void set_sleep();
+
+    /**
+     * @brief Clear the screen.
+     *
+     * This must be done when the controller is started, when the controller
+     * wakes from sleep mode and every once in a while when using A2 fast update
+     * mode.
+     */
     void clear_screen();
+
+    /**
+     * @brief Start copying an image to the controller.
+     * @param area The dimensions of the image.
+     * @param target_memory_address The target memory address to store the image at.
+     * @param rotate The hardware rotation associated with the image.
+     * @param pixel_format The pixel format of the data.
+     */
     void load_image_start(IT8951Area& area, uint32_t target_memory_address, it8951_rotate_t rotate,
                           it8951_pixel_format_t pixel_format);
+
+    /**
+     * @brief Transfer an SPI buffer to the controller.
+     * @param len The number of bytes in the SPI buffer to transfer.
+     */
     void load_image_flush_buffer(size_t len);
+
+    /**
+     * @brief Signal that the whole image has been copied.
+     */
     void load_image_end();
+
+    /**
+     * @brief Display an image on the screen.
+     * @param area The area to show the image.
+     * @param target_memory_address The location where the image is stored.
+     * @param pixel_format The pixel format of the image.
+     * @param mode The mode used to show the image.
+     */
     void display_area(IT8951Area& area, uint32_t target_memory_address, it8951_pixel_format_t pixel_format,
                       it8951_display_mode_t mode);
 
